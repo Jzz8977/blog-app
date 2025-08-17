079a6bb22e3af93b9017bc29dc9b553308f6b978
Jzz8977 <jzz8977@163.com>
2025-08-17 11:34:16 +0800

    commit api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Base configuration
-const API_BASE_URL = '/api'
+const API_BASE_URL = 'http://23.91.96.52/api'
 
 // Create axios instance
 const api = axios.create({
